492491a16e1816c2e971bc253bde46031efa61aa
Istiyaq123 <ahmedistiyaqqq03@gmail.com>
2020-01-16 17:51:47 +0000

    done

---
+++ app.js
@@ -0,0 +1,18 @@
+function onload() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "./video.mp4");
+    xhr.responseType = "arraybuffer";
+
+    xhr.onload = function (e) {
+        var blob = new Blob([xhr.responseType]);
+        var url = URL.createObjectURL(blob);
+
+        console.log(url);
+
+        var vid = document.getElementById("video");
+        vid.src = url;
+    }
+
+    xhr.send();
+}
